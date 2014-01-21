34c8167b5eb6c9dfaa885a29e83ced0637160fd9
SeungyoupBaek <bsy8220@naver.com>
2014-01-21 19:09:26 +0900

    Facebook SDK 이용하여 친구 목록 띄우기 완료하였습니다.

--- FacebookSdkFriendList/ViewController.m
+++ FacebookSdkFriendList/ViewController.m
@@ -7,16 +7,52 @@
 //
 
 #import "ViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSArray *_friends;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _friends.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FRIEND_CELL"];
+    NSDictionary<FBGraphUser> *friendList;
+    friendList = _friends[indexPath.row];
+    
+    cell.textLabel.text = friendList.name;
+    
+    return cell;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _friends = [[NSArray alloc] init];
+    FBLoginView *loginView = [[FBLoginView alloc] init];
+    // Align the button in the center horizontally
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 5);
+    [self.view addSubview:loginView];
+    
+    FBRequest* friendsRequest = [FBRequest requestForMyFriends];
+    [friendsRequest startWithCompletionHandler: ^(FBRequestConnection *connection,
+                                                  NSDictionary* result,
+                                                  NSError *error) {
+        _friends = [result objectForKey:@"data"];
+        NSLog(@"Found: %d friends", (int)_friends.count);
+        for (NSDictionary<FBGraphUser>* friend in _friends) {
+            NSLog(@"I have a friend named %@ with id %@", friend.name, friend.id);
+        }
+        [self.table reloadData];
+    }];
+
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
